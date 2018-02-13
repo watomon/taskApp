af569b3f58b5c1c1748331a325ab65ae141fcb52
watomon <watomon1@gmail.com>
2018-02-13 13:32:19 +0900

    finish UI parts

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class InputViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
